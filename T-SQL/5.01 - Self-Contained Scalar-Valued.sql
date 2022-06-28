b89aed3dcadb13bf873c6ea546210eb5af55867d
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-06-28 23:25:11 +0430

    Self Contained

---
+++ T-SQL/5.01 - Self-Contained Scalar-Valued.sql
@@ -0,0 +1,167 @@
+﻿
+USE Test_DB;
+GO
+
+/*
+WHERE مستقل در بخش Subquery
+*/
+
+-- TOP جدید‌ترین سفارش ثبت‌شده با استفاده از فیلتر
+SELECT 
+	TOP (1) EmployeeID,
+	CustomerID,
+	OrderID,
+	MAX(OrderDate) AS NewOrders
+FROM dbo.Orders
+GROUP BY OrderID, OrderDate, EmployeeID, CustomerID
+ORDER BY OrderID DESC;
+GO
+
+-- OFFSET جدید‌ترین سفارش ثبت‌شده با استفاده از فیلتر
+SELECT
+	EmployeeID,
+	CustomerID,
+	OrderID,
+	MAX(OrderDate) AS NewOrders
+FROM dbo.Orders
+GROUP BY OrderID, OrderDate, EmployeeID, CustomerID
+ORDER BY OrderID DESC
+OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY;
+GO
+
+-- جدید‌ترین سفارش ثبت‌شده با استفاده از متغیر
+--DECLARE @MaxID AS INT = (SELECT MAX(OrderID) FROM dbo.Orders);
+--SELECT
+--	EmployeeID, CustomerID, OrderID
+--FROM dbo.Orders
+--	WHERE OrderID = @MaxID;
+--GO
+
+-- مستقل Subquery جدید‌ترین سفارش ثبت‌شده با استفاده از
+SELECT
+	EmployeeID, CustomerID, OrderID, OrderDate
+FROM dbo.Orders
+	WHERE OrderID = (SELECT MAX(OrderID) FROM dbo.Orders);
+GO
+--------------------------------------------------------------------
+
+/*
+SELECT مستقل در بخش Subquery
+*/
+
+-- .تعداد سفارش مشتریانی که درخواست سفارش داشته‌اند
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+GROUP BY CustomerID;
+GO
+
+-- ???
+-- تعداد سفارش هر مشتری به‌همراه تعداد کل سفارشات موجود
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num,
+	SUM(COUNT(OrderID)) AS Total -- Cannot perform an aggregate function on an expression containing an aggregate or a subquery.
+FROM dbo.Orders
+GROUP BY CustomerID;
+GO 
+
+/*
+نمایش تعداد سفارش‌های هر مشتری به‌همراه تعداد کل سفارشات تمامی مشتریان
+*/
+
+DECLARE @Num INT = (SELECT COUNT(OrderID) FROM dbo.Orders);
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num,
+	@Num AS Total
+FROM dbo.Orders
+GROUP BY CustomerID;
+GO
+
+-- .مستقل Subquery تعداد سفارش هر مشتری به‌همراه تعداد کل سفارش ثبت‌شده با استفاده از
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num,
+	(SELECT COUNT(OrderID) FROM dbo.Orders) AS Total
+FROM dbo.Orders
+GROUP BY CustomerID;
+GO
+--------------------------------------------------------------------
+
+/*
+کوئری‌ای بنویسید که علاوه بر تعداد سفارش ثبت‌شده توسط
+هر کارمند، جدید‌ترین و قدیمی‌ترین سفارش ثبت‌شده در میان
+.تمامی سفارشات از تمامی کارمندان را هم نمایش دهد
+*/
+-- مستقل Subquery با استفاده از
+SELECT
+	EmployeeID,
+	COUNT(OrderID) AS Num,
+	(SELECT MAX(OrderDate) FROM dbo.Orders) AS MaxOrders,
+	(SELECT MIN(OrderDate) FROM dbo.Orders) AS MinOrders
+FROM dbo.Orders
+GROUP BY EmployeeID;
+GO
+
+-- بررسی تفاوت این کوئری با کوئری بالا
+SELECT
+	EmployeeID,
+	COUNT(OrderID) AS Num,
+	MAX(OrderDate) AS MaxOrders,
+	MIN(OrderDate) AS MinOrders
+FROM dbo.Orders
+GROUP BY EmployeeID;
+GO
+--------------------------------------------------------------------
+
+/*
+:نکته بسیار مهم
+.مستقل تک‌مقدار می‌بایست همواره فقط یک مقدار را برگرداند Subquery
+*/
+
+-- .کارمندانی که نام‌خانوادگی آن‌ها با حرف پ آغاز می‌شود
+SELECT * FROM dbo.Employees
+	WHERE LastName LIKE N'پ%';
+GO
+
+/*
+تمامی سفارشات ثبت‌شده توسط کارمندانی که
+.نام‌خانوادگی آن‌ها با کاراکتر 'پ' شروع شده باشد
+JOIN با استفاده از
+*/
+SELECT
+	O.OrderID
+FROM dbo.Employees AS E
+JOIN dbo.Orders AS O
+	ON E.EmployeeID = O.EmployeeID
+	WHERE LastName LIKE N'پ%';
+GO
+/*
+تمامی سفارشات ثبت‌شده توسط کارمندانی که
+.نام‌خانوادگی آن‌ها با کاراکتر 'پ' شروع شده باشد
+مستقل تک‌مقدار Subquery با استفاده از
+*/
+SELECT
+	OrderID
+FROM dbo.Orders
+	WHERE EmployeeID = (SELECT EmployeeID FROM dbo.Employees
+							WHERE LastName LIKE N'پ%');
+GO
+
+-- .کارمندانی که نام‌خانوادگی آن‌ها با حرف ت آغاز می‌شود
+SELECT * FROM dbo.Employees
+	WHERE LastName LIKE N'ت%';
+GO
+
+/*
+تمامی سفارشات ثبت‌شده توسط کارمندانی که
+.نام‌خانوادگی آن‌ها با کاراکتر 'ت' شروع شده باشد
+*/
+SELECT
+	OrderID
+FROM dbo.Orders
+	WHERE EmployeeID = (SELECT EmployeeID FROM dbo.Employees
+							WHERE LastName LIKE N'ت%');
+GO
---
+++ T-SQL/5.02 - Self-Contained Multi-Valued.sql
@@ -0,0 +1,196 @@
+﻿USE Test_DB;
+GO
+
+/*
+Self-Contained Multi-Valued Subquery
+
+<scalar_expression> IN (<multivalued subquery>)
+*/
+
+-- نمایش تمامی مشتریان شهر‌های تهران و اصفهان
+SELECT
+	CustomerID, City
+FROM dbo.Customers
+	WHERE City IN (N'تهران',N'اصفهان');
+GO
+
+-- .تمامی سفارشاتی که توسط کارمندانی ثبت شده که نام‌خانوادگی آن‌ها با کاراکتر 'ت' شروع شده باشد
+SELECT
+	EmployeeID, OrderID
+FROM dbo.Orders
+	WHERE EmployeeID = (SELECT E.EmployeeID FROM dbo.Employees AS E 
+							WHERE E.lastname LIKE N'ت%'); -- کارمندان: تقوی / تهرانی
+GO
+
+-- Self-Contained Multi-Valued Subquery رفع مشکل کوئری بالا با استفاده از
+SELECT
+	EmployeeID, OrderID
+FROM dbo.Orders
+	WHERE EmployeeID IN (SELECT E.EmployeeID FROM dbo.Employees AS E 
+							WHERE E.lastname LIKE N'ت%'); -- کارمندان: تقوی / تهرانی
+GO
+--------------------------------------------------------------------
+
+/*
+.لیست مشتریانی که سفارش ثبت نکرده‌اند
+*/
+-- LEFT JOIN با استفاده از
+SELECT C.* FROM dbo.Customers AS C
+LEFT JOIN dbo.Orders AS O
+	ON C.CustomerID = O.CustomerID
+	WHERE O.OrderID IS NULL;
+GO
+
+-- ???
+SELECT C.* FROM dbo.Customers AS C
+LEFT JOIN dbo.Orders AS O
+	ON C.CustomerID = O.CustomerID
+	AND O.OrderID IS NULL;
+GO
+
+-- Self-Contained Multi-Valued Subquery نوشتن کوئری بالا با استفاده از
+SELECT * FROM dbo.Customers
+	WHERE CustomerID  NOT IN (SELECT CustomerID FROM dbo.Orders);
+GO
+
+-- ???
+SELECT * FROM dbo.Customers AS C
+	WHERE CustomerID NOT IN (SELECT DISTINCT CustomerID FROM dbo.Orders);
+GO
+--------------------------------------------------------------------
+
+/*
+DISTINCT مقایسه کوئری‌های مشابه در دیتابیسی با تعداد رکورد زیاد و عدم تاثیر
+*/
+USE AdventureWorks2017;
+GO
+
+SELECT *
+FROM [Production].[Product]
+	WHERE ProductID NOT IN (SELECT DISTINCT ProductID
+							FROM [Sales].[SalesOrderDetail]);
+GO
+
+SELECT *
+FROM [Production].[Product]
+	WHERE ProductID NOT IN (SELECT ProductID
+							FROM [Sales].[SalesOrderDetail]);
+GO
+--------------------------------------------------------------------
+
+USE Test_DB;
+GO
+
+/*
+مشاهده مشخصات تمامی شرکت‌هایی که فقط در
+.تاریخ 2016-05-05 درخواست سفارش نداشته‌اند 
+*/
+SELECT
+	CustomerID, CompanyName
+FROM dbo.Customers
+	WHERE CustomerID NOT IN (SELECT CustomerID FROM dbo.Orders
+								WHERE OrderDate = '2016-05-05');
+GO
+
+-- ???
+SELECT
+	CustomerID, CompanyName
+FROM dbo.Customers
+	WHERE CustomerID NOT IN (SELECT CustomerID FROM dbo.Orders
+								WHERE OrderDate <> '2016-05-05');
+GO
+--------------------------------------------------------------------
+
+/*
+IN با گزاره NULL رفتار
+*/
+
+-- .نمایش تمامی مشتریانی که در منطقه مرکز واقع شده‌اند
+SELECT * FROM dbo.Customers
+	WHERE Region IN (N'مرکز');
+GO
+
+--  .است NULL آن‌ها برابر با Region نمایش تمامی مشتریانی که مقدار فیلد 
+SELECT * FROM dbo.Customers
+	WHERE Region IN (NULL); -- Region = NULL
+GO
+
+/*
+روش اصلاح‌شده کوئری بالا
+.است NULL آن‌ها برابر با Region نمایش تمامی مشتریانی که مقدار فیلد 
+*/
+SELECT * FROM dbo.Customers
+	WHERE Region IS NULL;
+GO
+
+-- .قرار دارند NULL مشتریانی که در منطقه مرکز یا
+SELECT * FROM dbo.Customers
+	WHERE Region IN (N'مرکز', NULL);
+GO
+
+/*
+.معادل کوئری بالا است و هیچ تفاوتی در خروجی دیده نخواهد شد
+*/
+SELECT * FROM dbo.Customers
+	WHERE Region IN (N'مرکز');
+GO
+
+-- اصلاح دو کوئری اخیر
+SELECT * FROM dbo.Customers
+	WHERE Region = N'مرکز'
+	OR Region IS NULL;
+GO
+--------------------------------------------------------------------
+
+/*
+NOT IN با گزاره NULL رفتار
+*/
+
+-- .یا مرکز نباشد NULL آن‌ها برابر با Region نمایش تمامی مشتریانی که مقدار فیلد 
+SELECT * FROM dbo.Customers
+	WHERE Region NOT IN (N'مرکز',NULL)
+ORDER BY Region DESC;
+GO
+
+-- روش اصلاح‌شده کوئری بالا
+SELECT * FROM dbo.Customers
+	WHERE Region <> N'مرکز' AND Region IS NOT NULL
+ORDER BY Region DESC;
+GO
+
+-- .آن‌ها برابر با مرکز یا غرب نباشد Region نمایش تمامی مشتریانی که مقدار فیلد 
+SELECT * FROM dbo.Customers
+	WHERE Region NOT IN (N'مرکز',N'غرب')
+ORDER BY Region DESC;
+GO
+--------------------------------------------------------------------
+
+/*
+کوئری‌ای بنویسید که فقط مشخصات شرکت‌هایی رانمایش دهد که
+.سفارشات درخواست‌شده آن‌ها فرد بوده یا اصلا درخواست سفارش نداشته‌اند
+*/
+SELECT
+	DISTINCT CustomerID
+FROM dbo.Orders
+	WHERE OrderID % 2 = 0;
+GO
+
+SELECT
+	CompanyName, CustomerID
+FROM dbo.Customers
+	WHERE CustomerID NOT IN (SELECT DISTINCT CustomerID FROM dbo.Orders
+								WHERE OrderID % 2 = 0);
+GO
+
+/*
+روشی پیچیده‌تر، پرهزینه‌تر و مشابه برای نوشتن کوئری بالا
+که ناشی از عدم آگاهی از نحوه اجرای کوئری بالا است
+*/
+SELECT
+	DISTINCT C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+LEFT JOIN dbo.Orders AS O
+	ON C.CustomerID = O.CustomerID
+	WHERE C.CustomerID NOT IN (SELECT CustomerID FROM dbo.Orders 
+									WHERE OrderID % 2 = 0);
+GO

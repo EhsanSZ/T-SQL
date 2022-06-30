171ffc4ff925e0ef9aca3390fd2f56a930a5254e
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-06-30 22:34:54 +0430

    Questions

---
+++ T-SQL/5.05 - Questions (1).sql
@@ -0,0 +1,352 @@
+﻿
+USE Test_DB;
+GO
+
+/*
+Exersice 01
+.فهرست شرکت‌هایی که بیش از 10 سفارش درخواست داشته‌اند
+*/
+
+/*
+JOIN
+*/
+SELECT--In Class
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+JOIN dbo.Orders AS O
+	ON C.CustomerID = O.CustomerID
+GROUP BY C.CompanyName, C.CustomerID
+	HAVING COUNT(O.OrderID) > 10;
+GO
+
+
+/*
+Subquery (WHERE)
+
+Outer Query: Customers
+Subquery: Orders
+*/
+SELECT--In Class
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+	WHERE (SELECT COUNT(O.OrderID) FROM dbo.Orders AS O
+			WHERE O.CustomerID = C.CustomerID) > 10;
+GO
+
+
+/*
+Subquery (WHERE)
+
+Outer Query: Customers
+Subquery: Orders
+*/
+SELECT--In Class
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+	WHERE C.CustomerID = (SELECT O.CustomerID FROM dbo.Orders AS O
+							WHERE O.CustomerID = C.CustomerID
+						  GROUP BY O.CustomerID
+							HAVING COUNT(O.OrderID) > 10);
+GO
+
+
+/*
+Subquery (EXISTS)
+
+Outer Query: Customers
+Subquery: Orders
+*/
+SELECT--In Class
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+	WHERE EXISTS (SELECT 1 FROM dbo.Orders AS O
+					WHERE O.CustomerID = C.CustomerID
+					HAVING COUNT(O.OrderID) > 10);
+GO
+
+
+/*
+Subquery (IN)
+
+Outer Query: Customers
+Subquery: Orders
+*/
+SELECT--In Class
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+	WHERE C.CustomerID IN (SELECT O.CustomerID FROM dbo.Orders AS O
+						   GROUP BY O.CustomerID
+							HAVING COUNT(O.OrderID) > 10);
+GO
+
+
+/*
+Subquery (SELECT)
+
+Outer Query: Customers
+Subquery: Orders
+*/
+SELECT--In Class
+	C.CompanyName,
+	(SELECT O.CustomerID FROM dbo.Orders AS O
+		WHERE O.CustomerID = C.CustomerID
+	 GROUP BY O.CustomerID
+		HAVING COUNT(O.OrderID) > 10) AS CustomerID
+FROM dbo.Customers AS C;
+GO
+
+
+/*
+Subquery (SELECT)
+
+Outer Query: Orders
+Subquery: Customers
+*/
+SELECT--In Class
+	(SELECT C.CompanyName FROM dbo.Customers AS C
+		WHERE C.CustomerID = O.CustomerID) AS CompanyName,
+	O.CustomerID
+FROM dbo.Orders AS O
+GROUP BY O.CustomerID
+	HAVING COUNT(O.OrderID) > 10;
+GO
+--------------------------------------------------------------------
+
+/*
+Exersice 02
+.تعداد سفارش شرکت‌هایی که در استان زنجان واقع شده‌اند
+*/
+
+/*
+JOIN
+*/
+SELECT--In Class
+	C.CompanyName,
+	COUNT(O.OrderID) AS Num
+FROM dbo.Customers AS C
+LEFT JOIN dbo.Orders AS O
+	ON C.CustomerID = O.CustomerID
+	WHERE C.State = N'زنجان'
+GROUP BY C.CompanyName;
+GO
+
+-- ???
+SELECT
+	C.CompanyName,
+	COUNT(O.OrderID) AS Num
+FROM dbo.Customers AS C
+LEFT JOIN dbo.Orders AS O
+	ON C.CustomerID = O.CustomerID
+	AND C.State = N'زنجان'
+GROUP BY C.CompanyName;
+GO
+
+/*
+Subquery (SELECT)
+
+Outer Query: Customers
+Subquery: Orders
+*/
+SELECT--In Class
+	C.CompanyName,
+	(SELECT COUNT(O.OrderID) FROM dbo.Orders AS O
+		WHERE O.CustomerID = C.CustomerID) AS Num
+FROM dbo.Customers AS C
+	WHERE C.State = N'زنجان';
+GO
+
+
+-- ???
+SELECT
+	C.CompanyName,
+	(SELECT COUNT(*) FROM dbo.Orders AS O
+		WHERE O.CustomerID = C.CustomerID) AS Num
+FROM dbo.Customers AS C
+	WHERE C.State = N'زنجان';
+GO
+
+-- ???
+SELECT
+	C.CompanyName, 
+	(SELECT COUNT(OrderID) FROM dbo.Orders AS O 
+		WHERE O.CustomerID = C.CustomerID
+		AND C.State = N'زنجان') AS Num
+FROM dbo.Customers AS C;
+GO
+
+
+/*
+Subquery (SELECT)
+
+Outer Query: Orders
+Subquery: Customers
+*/
+SELECT--In Class
+	(SELECT C.CompanyName FROM dbo.Customers AS C
+		WHERE C.State = N'زنجان'
+		AND C.CustomerID = O.CustomerID) AS CompanyName,
+	COUNT(O.OrderID) AS Num
+FROM dbo.Orders AS O
+GROUP BY O.CustomerID;
+GO
+--------------------------------------------------------------------
+
+/*
+Exersice 03
+.محصولاتی که قیمت واحد آن‌ها از میانگین قیمت واحد تمامی محصولات بیشتر و یا با آن برابر باشد
+*/
+
+-- میانگین قیمت واحد تمامی محصولات
+SELECT AVG(UnitPrice) FROM dbo.Products;
+GO
+
+-- ???
+SELECT
+	ProductID, UnitPrice
+FROM dbo.Products
+GROUP BY ProductID
+	HAVING UnitPrice >= AVG(UnitPrice);
+GO
+
+-- ???
+SELECT
+	ProductID, UnitPrice
+FROM dbo.Products
+GROUP BY ProductID, UnitPrice
+	HAVING UnitPrice >= AVG(UnitPrice);
+GO
+
+-- Subquery (WHERE)
+SELECT--In Class
+	ProductID, UnitPrice
+FROM dbo.Products AS P
+	WHERE P.UnitPrice >= (SELECT AVG(UnitPrice) FROM dbo.Products);
+GO
+
+
+-- Subquery (IN)
+SELECT
+	ProductID, UnitPrice
+FROM dbo.Products AS P
+	WHERE P.ProductID IN (SELECT ProductID FROM dbo.Products AS P
+							WHERE P.UnitPrice >= (SELECT AVG(UnitPrice) FROM dbo.Products));
+GO
+
+
+-- Subquery (EXISTS)
+SELECT
+	ProductID, UnitPrice
+FROM dbo.Products AS P
+	WHERE EXISTS (SELECT 1 FROM dbo.Products AS P1
+					WHERE P1.UnitPrice >= (SELECT AVG(UnitPrice) FROM dbo.Products)
+					AND P1.ProductID = P.ProductID);
+GO
+--------------------------------------------------------------------
+
+/*
+Exersice 04
+.مشخصات کارمندی که تا به امروز کمترین تعداد ثبتِ سفارش را داشته است
+*/
+
+-- تعداد سفارشات ثبت‌شده توسط هر کارمند
+SELECT
+	EmployeeID,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+GROUP BY EmployeeID;
+GO
+
+-- .کارمندانی که کمترین ثبت‌سفارش داشته‌اند
+SELECT
+	TOP (1) WITH TIES EmployeeID,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+GROUP BY EmployeeID
+ORDER BY Num;
+GO
+
+-- JOIN
+SELECT--In Class
+	TOP (1) WITH TIES E.EmployeeID,
+	E.FirstName,
+	E.LastName
+FROM dbo.Orders AS O
+JOIN dbo.Employees AS E
+	ON E.EmployeeID = O.EmployeeID
+GROUP BY E.EmployeeID, E.FirstName, E.LastName
+ORDER BY COUNT(O.OrderID);
+GO
+
+
+/*
+Subquery (IN)
+
+Outer Query: Employees
+Subquery: Orders
+*/
+SELECT--In Class
+	E.EmployeeID , E.FirstName, E.LastName
+FROM dbo.Employees AS E
+	WHERE E.EmployeeID IN (SELECT TOP (1) WITH TIES O.EmployeeID FROM dbo.Orders AS O
+						   GROUP BY O.EmployeeID
+						   ORDER BY COUNT(O.OrderID));
+GO
+
+
+/*
+Subquery (WHERE)
+
+Outer Query: Employees
+Subquery: Orders
+
+روش خطرناک
+*/
+SELECT--In Class
+	EmployeeID, FirstName, LastName
+FROM dbo.Employees
+	WHERE EmployeeID = (SELECT
+							TOP (1) WITH TIES EmployeeID 
+						FROM dbo.Orders
+						GROUP BY EmployeeID
+						ORDER BY COUNT(OrderID));
+GO
+
+
+/*
+Subquery (SELECT)
+
+Outer Query: Orders
+Subquery: Employees
+*/
+SELECT--In Class
+	TOP (1) WITH TIES O.EmployeeID,
+	(SELECT E.FirstName FROM dbo.Employees AS E
+		WHERE E.EmployeeID = O.EmployeeID) AS FirstName,
+	(SELECT E.LastName FROM dbo.Employees AS E
+		WHERE E.EmployeeID = O.EmployeeID) AS LastName
+FROM dbo.Orders AS O
+GROUP BY O.EmployeeID
+ORDER BY COUNT(O.OrderID);
+GO
+
+
+/*
+Subquery (SELECT)
+
+Outer Query: Employees
+Subquery: Orders
+*/
+SELECT
+	TOP (1) WITH TIES E.EmployeeID, E.FirstName, E.LastName,
+	(SELECT COUNT(OrderID) FROM dbo.Orders AS O
+		WHERE E.EmployeeID = O.EmployeeID) AS Num 
+FROM dbo.Employees AS E
+ORDER BY Num;
+GO
+
+SELECT--In Class
+	TOP (1) WITH TIES E.EmployeeID, E.FirstName, E.LastName
+FROM dbo.Employees AS E
+ORDER BY (SELECT COUNT(OrderID) FROM dbo.Orders AS O
+			WHERE E.EmployeeID = O.EmployeeID);
+GO
---
+++ T-SQL/5.06 - Questions (2).sql
@@ -0,0 +1,167 @@
+﻿
+USE Test_DB;
+GO
+
+/*
+Exersice 01
+مشخصات شرکت‌هایی که حداقل در یکی از ماه‌های سال 2015 سفارش داشته‌اند
+.اما در سال 2016 هنوز درخواست سفارشی نداشته‌اند
+*/
+
+-- .همه مشتریانی که در سال 2015 ثبت‌سفارش داشته‌اند
+SELECT
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+	WHERE EXISTS (SELECT 1 FROM dbo.Orders AS O
+					WHERE O.CustomerID = C.CustomerID
+					AND YEAR(O.OrderDate) = 2015);
+GO
+
+-- .همه مشتریانی که در سال 2016 ثبت‌سفارش داشته‌اند
+SELECT
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+	WHERE EXISTS (SELECT 1 FROM dbo.Orders AS O
+					WHERE O.CustomerID = C.CustomerID
+					AND YEAR(O.OrderDate) = 2016);
+GO
+
+/*
+Set Operator های تنبل و استفاده از Developer رفتار
+*/
+SELECT
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+	WHERE EXISTS (SELECT 1 FROM dbo.Orders AS O
+					WHERE O.CustomerID = C.CustomerID
+					AND YEAR(O.OrderDate) = 2015)
+
+EXCEPT
+
+SELECT
+	C.CompanyName, C.CustomerID
+FROM dbo.Customers AS C
+	WHERE EXISTS (SELECT 1 FROM dbo.Orders AS O
+					WHERE O.CustomerID = C.CustomerID
+					AND YEAR(O.OrderDate) = 2016);
+GO
+
+
+-- ???
+SELECT
+	(SELECT C.CompanyName FROM dbo.Customers AS C
+		WHERE C.CustomerID = O.CustomerID) AS CompanyName,
+	O.CustomerID
+FROM dbo.Orders AS O
+	WHERE YEAR(O.OrderDate) = 2015
+	AND YEAR(O.OrderDate) <> 2016
+GROUP BY O.CustomerID;
+GO
+
+/*
+Subquery (EXISTS)
+
+Outer Query: Customers
+Subquery: Orders
+*/
+SELECT
+	C.CustomerID, C.CompanyName
+FROM dbo.Customers AS C
+	WHERE EXISTS (SELECT 1 FROM dbo.Orders AS O
+					WHERE C.CustomerID = O.CustomerID
+					AND YEAR(O.OrderDate) = 2015)
+	AND NOT EXISTS (SELECT 1 FROM dbo.Orders AS O
+						WHERE O.CustomerID = C.CustomerID
+						AND YEAR(OrderDate) = 2016);
+GO
+
+/*
+Subquery (IN)
+
+Outer Query: Customers
+Subquery: Orders
+*/
+SELECT
+	C.CustomerID, C.CompanyName
+FROM dbo.Customers AS C
+	WHERE C.CustomerID IN (SELECT O.CustomerID FROM dbo.Orders AS O
+							WHERE YEAR(O.OrderDate) = 2015)
+	AND C.CustomerID NOT IN (SELECT O.CustomerID FROM dbo.Orders AS O
+								WHERE YEAR(O.OrderDate) = 2016);
+GO
+--------------------------------------------------------------------
+
+/*
+Exersice 02
+
+.هستند A نمایش رکوردهایی که فقط در جدول
+*/
+
+DROP TABLE IF EXISTS A,B;
+GO
+
+CREATE TABLE A
+(
+	ID TINYINT
+);
+GO
+
+CREATE TABLE B
+(
+	ID TINYINT
+);
+GO
+
+INSERT INTO A
+	VALUES (1),(2),(3),(4);
+GO
+
+INSERT INTO B
+	VALUES (2),(NULL);
+GO 
+
+-- JOIN
+SELECT A.* FROM A
+JOIN B
+	ON A.ID <> B.ID;
+GO
+
+-- Subquery (EXISTS)
+SELECT * FROM A
+	WHERE EXISTS (SELECT 1 FROM B
+					WHERE B.ID <> A.ID);
+GO
+
+-- Subquery (NOT EXISTS)
+SELECT * FROM A
+	WHERE NOT EXISTS (SELECT 1 FROM B
+						WHERE B.ID = A.ID);
+GO
+
+-- Subquery (NOT IN) Without Checking
+SELECT * FROM A
+	WHERE A.ID NOT IN (SELECT ID FROM B);
+GO
+
+-- Subquery (NOT IN) With Checking
+SELECT
+	A.ID
+FROM A
+	WHERE A.ID NOT IN (SELECT ID FROM B
+						WHERE B.ID IS NOT NULL);
+GO
+
+-- Subquery (NOT IN) With Checking
+SELECT
+	A.ID
+FROM A
+	WHERE A.ID NOT IN (SELECT ISNULL(ID, '') FROM B);
+GO
+
+-- Subquery (NOT IN - Correlated)
+SELECT
+	A.ID
+FROM A
+	WHERE A.ID NOT IN (SELECT ID FROM B
+						WHERE B.ID = A.ID);
+GO

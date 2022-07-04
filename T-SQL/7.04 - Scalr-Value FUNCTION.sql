b6aac2c82ff6f2be3fb8319ef2be3cd8ad977b82
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-07-04 23:55:28 +0430

    Function

---
+++ T-SQL/7.04 - Scalr-Value FUNCTION.sql
@@ -0,0 +1,70 @@
+﻿
+USE Test_DB;
+GO
+
+/*
+Scalar-Value FUNCTION
+
+CREATE FUNCTION FUNCTION_Name
+	({@Parameter [AS] type[=default]}[,...n])
+	RETURNS Type
+AS
+BEGIN
+	Function_Body
+	RETURN Expression
+END
+*/
+
+DROP FUNCTION IF EXISTS dbo.Abbreviation;
+GO
+
+-- تعریف تابع
+CREATE FUNCTION dbo.Abbreviation (@FirstName NVARCHAR(50),@LastName NVARCHAR(100))
+RETURNS NCHAR(3)
+AS
+BEGIN
+	DECLARE @Output NCHAR(3)
+	SET @Output = LEFT(@FirstName,1) + '.' + LEFT(@LastName,1)
+	RETURN @Output
+END
+GO
+
+-- استفاده از تابع در حالت عادی
+SELECT dbo.Abbreviation(N'سعید',N'پاشایی');
+GO
+
+/*
+استفاده از تابع برای بازیابی رکوردهای جداول
+
+ترکیب نام و نام‌خانوادگی کارمندان
+*/
+SELECT
+	EmployeeID, FirstName, LastName,
+	dbo.Abbreviation(FirstName,LastName)
+FROM dbo.Employees;
+GO
+--------------------------------------------------------------------
+
+/*
+.تابعی که سن کارمندان را نمایش دهد
+*/
+
+DROP FUNCTION IF EXISTS dbo.GetAge;
+GO
+
+CREATE FUNCTION dbo.GetAge (@BirthDate DATE)
+RETURNS TINYINT
+AS
+BEGIN
+	--DECLARE @Age TINYINT
+	--SET @Age = DATEDIFF(YEAR,@BirthDate,GETDATE())
+	--RETURN @Age
+	RETURN DATEDIFF(YEAR,@BirthDate,GETDATE())
+END
+GO
+
+SELECT
+	EmployeeID, FirstName, LastName,
+	dbo.GetAge(Birthdate) AS Age
+FROM dbo.Employees;
+GO
---
+++ T-SQL/7.05 - Inline Table-Value FUNCTION.sql
@@ -0,0 +1,94 @@
+﻿
+USE Test_DB;
+GO
+
+/*
+Inline Table-Value FUNCTION
+*/
+
+--CREATE FUNCTION Statement (Inline Table Valued Function)
+-- Parameterized View
+/*
+CREATE FUNCTION  function_name 
+    ( [ { @parameter_name [AS] scalar_parameter_data_type [ = default ] } [ ,...n ] ] ) 
+RETURNS TABLE 
+[ WITH < function_option > [ [,] ...n ] ] 
+[ AS ] 
+RETURN [ ( ] select-stmt [ ) ] 
+*/
+
+DROP VIEW IF EXISTS dbo.Customers_Info;
+GO
+
+--.ای كه اطلاعات شرکت و سفارش‌هاي مشتريان را نمايش مي‌دهد VIEW ایجاد
+CREATE VIEW dbo.Customers_Info
+AS
+	SELECT
+		C.CompanyName, C.City ,O.OrderID, O.OrderDate    
+	FROM dbo.Customers AS C
+	JOIN dbo.Orders AS O
+		ON C.CustomerID=O.CustomerID; 
+GO
+
+--نمايش سفارش هاي مشتريان تهرانی
+SELECT * FROM dbo.Customers_Info
+	WHERE City= N'تهران';
+GO
+
+/*
+Inline Table Valued Function
+FUNCTION بالا با استفاده از VIEW ایجاد
+*/
+
+DROP FUNCTION IF EXISTS dbo.Func_Customers_Info;
+GO
+
+CREATE FUNCTION dbo.Func_Customers_Info (@City NVARCHAR(50))
+RETURNS TABLE
+AS
+RETURN
+	SELECT
+		C.CompanyName, C.City ,O.OrderID, O.OrderDate    
+	FROM dbo.Customers AS C
+	JOIN dbo.Orders AS O
+		ON C.CustomerID=O.CustomerID
+		WHERE C.City = @City;
+GO
+
+-- فراخوانی تابع
+SELECT * FROM dbo.Func_Customers_Info(N'تهران');
+GO
+
+/*
+JOIN مشاهده جزئیات سفارش کارمندان از طریق
+OrderDetails با جدول Func_Customers_Info میان تابع
+*/
+SELECT * FROM dbo.Func_Customers_Info (N'تهران') AS F
+JOIN dbo.OrderDetails AS OD
+	ON F.OrderID = OD.OrderID;
+GO
+--------------------------------------------------------------------
+
+/*
+.تابعی که تعداد مشخصی از جدیدترین سفارش یک مشتری را نمایش دهد
+پارامترهای ورودی: کد مشتری - عدد مربوط به تعداد سفارشات جدید
+*/
+
+DROP FUNCTION IF EXISTS dbo.Top_Orders;
+GO
+
+-- ایجاد تابع
+CREATE FUNCTION dbo.Top_Orders (@C_ID INT,@N TINYINT)
+RETURNS TABLE
+AS
+RETURN
+	SELECT
+		TOP (@N) OrderID, CustomerID, OrderDate
+	FROM dbo.Orders
+		WHERE CustomerID = @C_ID
+	ORDER BY OrderDate DESC;
+GO
+
+-- فراخوانی تابع
+SELECT * FROM dbo.Top_Orders(1,5);
+GO

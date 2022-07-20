67aa813b4d1c7468f461645b51b800e2d522e11b
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-07-20 23:34:27 +0430

    Apply

---
+++ T-SQL/8.06 - Multi Statement Table-Value FUNCTION.sql
@@ -0,0 +1,34 @@
+﻿USE Test_DB;
+GO
+
+/*
+Multi Statement Table-Value FUNCTION
+*/
+
+--CREATE FUNCTION Statement (Multi Statement Table Valued)
+/*
+CREATE FUNCTION [owner_name.] function_name 
+    ({@parameter [AS] type [= default]}[,...n ]) 
+	RETURNS @return_variable TABLE < table_type_definition > 
+AS
+	BEGIN 
+	    function_body 
+	    RETURN
+	END
+*/
+
+DROP FUNCTION IF EXISTS dbo.Multi_Statement_Table_Valued;
+GO
+
+CREATE FUNCTION dbo.Multi_Statement_Table_Valued()
+	RETURNS @Tbl TABLE (Col1 INT, Col2 NVARCHAR(100))
+AS
+	BEGIN
+		INSERT @Tbl 
+			VALUES (1,'HELLO'),(2,'SQL')
+		RETURN;
+	END
+GO
+
+SELECT * FROM dbo.Multi_Statement_Table_Valued();
+GO
---
+++ T-SQL/8.07 - APPLY.sql
@@ -0,0 +1,84 @@
+﻿USE Test_DB;
+GO
+
+SELECT
+	E1.EmployeeID AS E1_Emp, E2.EmployeeID AS E2_Emp
+FROM dbo.Employees AS E1
+CROSS APPLY
+	 dbo.Employees AS E2; -- .را دارد CROSS JOIN همان کارکرد
+GO
+--------------------------------------------------------------------
+
+/*
+APPLY & Derived Table
+*/
+
+-- نمایش 3 سفارش اخیر مشتری 1 
+SELECT
+	TOP (3) O.CustomerID, O.OrderID, o.OrderDate
+FROM dbo.Orders AS O
+	WHERE O.CustomerID = 1 -- 1 ... 91
+ORDER BY O.OrderDate DESC;
+GO
+
+/*
+نمایش 3 سفارش اخیر هر مشتری 
+APPLY در سمت راست عملگر Derived Table با استفاده از
+*/
+SELECT
+	C.CustomerID, C.CompanyName, Tmp.OrderID
+FROM dbo.Customers AS C
+CROSS APPLY
+	(SELECT TOP(3) O.OrderID FROM dbo.Orders AS O
+		WHERE O.CustomerID = C.CustomerID) AS Tmp;
+GO
+
+
+/*
+نمایش سه سفارش اخیر هر مشتری حتی مشتریان فاقد سفارش 
+APPLY در سمت راست عملگر Derived Table با استفاده از
+*/
+SELECT
+	C.CustomerID, C.CompanyName, Tmp.OrderID, Tmp.OrderDate
+FROM dbo.Customers AS C
+OUTER APPLY
+	(SELECT TOP(3) O.OrderID, O.OrderDate FROM dbo.Orders AS O
+		WHERE O.CustomerID = C.CustomerID) AS Tmp;
+GO
+--------------------------------------------------------------------
+
+/*
+APPLY & TVF
+*/
+
+/*
+تابعی که قبلا نوشتم
+
+DROP FUNCTION IF EXISTS dbo.Top_Orders;
+GO
+
+CREATE FUNCTION dbo.Top_Orders(@CustID AS INT, @n AS TINYINT)
+RETURNS TABLE
+AS
+RETURN
+	SELECT
+		TOP (@n) OrderID, CustomerID, OrderDate
+	FROM dbo.Orders
+		WHERE CustomerID = @CustID
+	ORDER BY OrderDate DESC, OrderID DESC;
+GO
+*/
+
+-- فراخوانی تابع
+SELECT * FROM dbo.Top_Orders(1,5);
+GO
+
+/*
+نمایش سه سفارش اخیر هر مشتری 
+APPLY در سمت راست عملگر TVF با استفاده از
+*/
+SELECT
+	C.CustomerID, C.CompanyName, T.OrderID, T.OrderDate
+FROM dbo.Customers AS C
+CROSS APPLY dbo.Top_Orders(C.CustomerID,4) AS T;
+GO
